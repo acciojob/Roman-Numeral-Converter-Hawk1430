e4a83d0567e6becee01b672fbb2ddbbe38282b54
Prashant Raj <pr2125116@gmail.com>
2025-04-18 07:29:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,11 +10,37 @@ function convertToRoman(num) {
     };
 
   //your code here
+	const romanMap = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
+
+  let result = '';
+
+  for (let i = 0; i < romanMap.length; i++) {
+    while (num >= romanMap[i][1]) {
+      result += romanMap[i][0];
+      num -= romanMap[i][1];
+    }
+  }
+
+  return result;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
